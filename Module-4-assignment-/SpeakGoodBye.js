48ce86fa9977c37f16906f6d2c0364c1fa04834a
timymus <55903864+timymus@users.noreply.github.com>
2024-07-12 14:39:54 -0400

    I replaced module-4-assigment by Module-4-solution

--- Module-4-assignment-/SpeakGoodBye.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-  var byeSpeaker = {};
-  var speakWord = "Good bye ";
-  byeSpeaker.speak = function (name) {
-      console.log(speakWord + name);
-  };
-  window.byeSpeaker = byeSpeaker;
-})(window);
